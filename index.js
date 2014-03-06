eeada1787199444f6c6881fb31db781e41bc3949
uchida <uchida.atsushi.8@gmail.com>
2014-03-07 01:18:02 +0900

    add marker. close #4

--- index.js
+++ index.js
@@ -2,13 +2,24 @@
 ///<reference path="typings/googlemaps/google.maps.d.ts" />
 var YEmergency;
 (function (YEmergency) {
+    function createMarker(map, latLang, text) {
+        new google.maps.Marker({
+            position: latLang,
+            map: map,
+            title: text
+        });
+    }
+    YEmergency.createMarker = createMarker;
+
     function showPosition(position) {
+        var MyPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
         var mapOptions = {
-            center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+            center: MyPosition,
             zoom: 13,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+        createMarker(map, MyPosition, "You are here");
     }
     YEmergency.showPosition = showPosition;
 })(YEmergency || (YEmergency = {}));
--- index.ts
+++ index.ts
@@ -2,13 +2,24 @@
 ///<reference path="typings/googlemaps/google.maps.d.ts" />
 
 module YEmergency {
+
+    export function createMarker(map, latLang, text) {
+        new google.maps.Marker({
+            position: latLang,
+            map: map,
+            title: text
+        });
+    }
+
     export function showPosition(position) {
+        var MyPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
         var mapOptions = {
-            center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+            center: MyPosition,
             zoom: 13,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+        createMarker(map, MyPosition, "You are here");
     }
 }
 
